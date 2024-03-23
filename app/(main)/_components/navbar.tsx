ffd70a92111472676a5b125cdbd7ddeee1b62450
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2024-03-23 14:42:47 +0530

    add banner

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -7,6 +7,7 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { MenuIcon } from "lucide-react";
 import { Title } from "./title";
+import Banner from "./banner";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -46,6 +47,8 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
           <Title initialData={document} />
         </div>
       </nav>
+
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
